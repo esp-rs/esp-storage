3e0ba995d3e46e0aef254966234648444436bfb2
K <kayo@illumium.org>
2023-05-09 13:36:24 +0500

    Rework crate
    - Add common module for storage impls
    
      I added internal fns to FlashStorage itself.
      Also I added bounds and alignment checks.
    
      I fixed error type by adding possible reasons.
      Also I added check_rc fn for converting convert return code to result.
    
    - Add ReadStorage/Storage traits impls as a storage feature
    
      Also I made some optimizations and code cleanups.
    
      In particular to speed up read of small pieces I calculates length of buffer
      in a word aligned manner instead of reading overall sector.
    
    - Add ReadNorFlash/NorFlash traits impls as a nor-flash feature
    
    - Add support bytewise read feature to ReadNorFlash
    
      This feature is pretty useful because some crates (like sequential-storage) does not support non-bytewise reads.
    
    - Force sector buffer to be word aligned

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-#![no_std]
+#![cfg_attr(not(all(test, feature = "emulation")), no_std)]
 
-use embedded_storage::{ReadStorage, Storage};
-
+#[cfg(not(feature = "emulation"))]
 #[cfg_attr(feature = "esp32c2", path = "esp32c2.rs")]
 #[cfg_attr(feature = "esp32c3", path = "esp32c3.rs")]
 #[cfg_attr(feature = "esp32c6", path = "esp32c6.rs")]
@@ -23,186 +22,29 @@ use embedded_storage::{ReadStorage, Storage};
 )]
 mod chip_specific;
 
-const FLASH_SECTOR_SIZE: u32 = 4096;
+#[cfg(feature = "emulation")]
+#[path = "stub.rs"]
+mod chip_specific;
 
-#[derive(Debug)]
-pub enum FlashStorageError {
-    Other(i32),
-}
+#[cfg(any(feature = "storage", feature = "nor-flash"))]
+mod common;
 
-#[derive(Debug)]
-pub struct FlashStorage {
-    capacity: usize,
-    unlocked: bool,
-}
+#[cfg(any(feature = "storage", feature = "nor-flash"))]
+pub use common::{FlashStorage, FlashStorageError};
 
-impl FlashStorage {
-    pub fn new() -> FlashStorage {
-        let mut storage = FlashStorage {
-            capacity: 0,
-            unlocked: false,
-        };
+#[cfg(any(feature = "storage", feature = "nor-flash"))]
+use common::FlashSectorBuffer;
 
-        #[cfg(any(
-            feature = "esp32c3",
-            feature = "esp32s3",
-            feature = "esp32h2",
-            feature = "esp32c6",
-            feature = "esp32c2"
-        ))]
-        const ADDR: u32 = 0x0000;
-        #[cfg(not(any(
-            feature = "esp32c3",
-            feature = "esp32s3",
-            feature = "esp32h2",
-            feature = "esp32c6",
-            feature = "esp32c2"
-        )))]
-        const ADDR: u32 = 0x1000;
+#[cfg(feature = "storage")]
+mod storage;
 
-        let mut buffer = [0u8; 8];
-        storage.read(ADDR, &mut buffer).ok();
-        let mb = match buffer[3] & 0xf0 {
-            0x00 => 1,
-            0x10 => 2,
-            0x20 => 4,
-            0x30 => 8,
-            0x40 => 16,
-            _ => 0,
-        };
-        storage.capacity = mb * 1024 * 1024;
+#[cfg(feature = "nor-flash")]
+mod nor_flash;
 
-        storage
-    }
-}
+#[cfg(feature = "low-level")]
+pub mod ll;
 
-impl Default for FlashStorage {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-#[inline(never)]
-#[link_section = ".rwtext"]
-fn internal_read(offset: u32, bytes: &mut [u8]) -> Result<(), FlashStorageError> {
-    if bytes.len() % 4 != 0 {
-        return Err(FlashStorageError::Other(9999)); // TODO make this work - shouldn't be a requirement
-    }
-
-    let res = chip_specific::esp_rom_spiflash_read(
-        offset,
-        bytes.as_ptr() as *mut u8 as *mut u32,
-        bytes.len() as u32,
-    );
-
-    if res != 0 {
-        Err(FlashStorageError::Other(res))
-    } else {
-        Ok(())
-    }
-}
-
-#[inline(never)]
-#[link_section = ".rwtext"]
-fn internal_write(
-    storage: &mut FlashStorage,
-    offset: u32,
-    bytes: &[u8],
-) -> Result<(), FlashStorageError> {
-    if bytes.len() % 4 != 0 {
-        return Err(FlashStorageError::Other(9999)); // TODO make this work - shouldn't be a requirement
-    }
-
-    if !storage.unlocked {
-        if chip_specific::esp_rom_spiflash_unlock() != 0 {
-            return Err(FlashStorageError::Other(9998));
-        }
-        storage.unlocked = true;
-    }
-
-    let res = chip_specific::esp_rom_spiflash_erase_sector(offset / FLASH_SECTOR_SIZE);
-    if res != 0 {
-        return Err(FlashStorageError::Other(res));
-    }
-
-    let res = chip_specific::esp_rom_spiflash_write(
-        offset,
-        bytes.as_ptr() as *const u32,
-        bytes.len() as u32,
-    );
-
-    if res != 0 {
-        Err(FlashStorageError::Other(res))
-    } else {
-        Ok(())
-    }
-}
-
-impl ReadStorage for FlashStorage {
-    type Error = FlashStorageError;
-
-    fn read(&mut self, offset: u32, bytes: &mut [u8]) -> Result<(), Self::Error> {
-        let mut sector_start = (offset / FLASH_SECTOR_SIZE) * FLASH_SECTOR_SIZE;
-        let mut data_offset = offset - sector_start;
-        let mut dst_offset = 0;
-        loop {
-            let mut sector_data = [0u8; FLASH_SECTOR_SIZE as usize];
-            internal_read(sector_start, &mut sector_data)?;
-
-            let len = u32::min(
-                FLASH_SECTOR_SIZE - data_offset,
-                (bytes.len() - dst_offset) as u32,
-            );
-
-            bytes[dst_offset..][..len as usize]
-                .copy_from_slice(&sector_data[data_offset as usize..][..len as usize]);
-
-            sector_start += FLASH_SECTOR_SIZE;
-            data_offset = 0;
-            dst_offset += len as usize;
-
-            if dst_offset >= bytes.len() {
-                break Ok(());
-            }
-        }
-    }
-
-    /// The SPI flash size is configured by writing a field in the software bootloader image header.
-    /// This is done during flashing in espflash / esptool.
-    fn capacity(&self) -> usize {
-        self.capacity
-    }
-}
-
-impl Storage for FlashStorage {
-    fn write(&mut self, offset: u32, bytes: &[u8]) -> Result<(), Self::Error> {
-        let mut sector_start = (offset / FLASH_SECTOR_SIZE) * FLASH_SECTOR_SIZE;
-        let mut data_offset = offset - sector_start;
-        let mut dst_offset = 0;
-        loop {
-            let mut sector_data = [0u8; FLASH_SECTOR_SIZE as usize];
-            internal_read(sector_start, &mut sector_data)?;
-
-            let len = u32::min(
-                FLASH_SECTOR_SIZE - data_offset,
-                (bytes.len() - dst_offset) as u32,
-            );
-
-            sector_data[data_offset as usize..][..len as usize]
-                .copy_from_slice(&bytes[dst_offset..][..len as usize]);
-            internal_write(self, sector_start, &sector_data)?;
-
-            sector_start += FLASH_SECTOR_SIZE;
-            data_offset = 0;
-            dst_offset += len as usize;
-
-            if dst_offset >= bytes.len() {
-                break Ok(());
-            }
-        }
-    }
-}
-
+#[cfg(not(feature = "emulation"))]
 #[inline(always)]
 #[link_section = ".rwtext"]
 fn maybe_with_critical_section<R>(f: impl FnOnce() -> R) -> R {
@@ -213,5 +55,7 @@ fn maybe_with_critical_section<R>(f: impl FnOnce() -> R) -> R {
     f()
 }
 
-#[cfg(feature = "low-level")]
-pub mod ll;
+#[cfg(feature = "emulation")]
+fn maybe_with_critical_section<R>(f: impl FnOnce() -> R) -> R {
+    f()
+}
---
+++ src/nor_flash.rs
@@ -0,0 +1,196 @@
+use crate::{FlashSectorBuffer, FlashStorage, FlashStorageError};
+use core::{
+    mem::MaybeUninit,
+    ops::{Deref, DerefMut},
+};
+use embedded_storage::nor_flash::{
+    ErrorType, NorFlash, NorFlashError, NorFlashErrorKind, ReadNorFlash,
+};
+
+#[repr(align(4))]
+struct FlashWordBuffer {
+    data: [u8; FlashStorage::WORD_SIZE as usize],
+}
+
+impl Deref for FlashWordBuffer {
+    type Target = [u8; FlashStorage::WORD_SIZE as usize];
+
+    fn deref(&self) -> &Self::Target {
+        &self.data
+    }
+}
+
+impl DerefMut for FlashWordBuffer {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.data
+    }
+}
+
+impl FlashStorage {
+    #[inline(always)]
+    fn is_word_aligned(bytes: &[u8]) -> bool {
+        // TODO: Use is_aligned_to when stabilized (see `pointer_is_aligned`)
+        (unsafe { bytes.as_ptr().offset_from(core::ptr::null()) }) % Self::WORD_SIZE as isize == 0
+    }
+}
+
+impl NorFlashError for FlashStorageError {
+    fn kind(&self) -> NorFlashErrorKind {
+        match self {
+            Self::NotAligned => NorFlashErrorKind::NotAligned,
+            Self::OutOfBounds => NorFlashErrorKind::OutOfBounds,
+            _ => NorFlashErrorKind::Other,
+        }
+    }
+}
+
+impl ErrorType for FlashStorage {
+    type Error = FlashStorageError;
+}
+
+impl ReadNorFlash for FlashStorage {
+    #[cfg(not(feature = "bytewise-read"))]
+    const READ_SIZE: usize = Self::WORD_SIZE as _;
+
+    #[cfg(feature = "bytewise-read")]
+    const READ_SIZE: usize = 1;
+
+    fn read(&mut self, offset: u32, bytes: &mut [u8]) -> Result<(), Self::Error> {
+        self.check_alignment::<{ Self::READ_SIZE as _ }>(offset, bytes.len())?;
+        self.check_bounds(offset, bytes.len())?;
+
+        #[cfg(feature = "bytewise-read")]
+        let (offset, bytes) = {
+            let byte_offset = (offset % Self::WORD_SIZE) as usize;
+            if byte_offset > 0 {
+                let mut word_buffer = MaybeUninit::<FlashWordBuffer>::uninit();
+                let word_buffer = unsafe { word_buffer.assume_init_mut() };
+
+                let offset = offset - byte_offset as u32;
+                let length = bytes.len().min(word_buffer.len() - byte_offset);
+
+                self.internal_read(offset, &mut word_buffer[..])?;
+                bytes[..length].copy_from_slice(&word_buffer[byte_offset..][..length]);
+
+                (offset + Self::WORD_SIZE, &mut bytes[length..])
+            } else {
+                (offset, bytes)
+            }
+        };
+
+        if Self::is_word_aligned(bytes) {
+            // Bytes buffer is word-aligned so we can read directly to it
+            for (offset, chunk) in (offset..)
+                .step_by(Self::SECTOR_SIZE as _)
+                .zip(bytes.chunks_mut(Self::SECTOR_SIZE as _))
+            {
+                // Chunk already is word aligned so we can read directly to it
+                #[cfg(not(feature = "bytewise-read"))]
+                self.internal_read(offset, chunk)?;
+
+                #[cfg(feature = "bytewise-read")]
+                {
+                    let length = chunk.len();
+                    let byte_length = length % Self::WORD_SIZE as usize;
+                    let length = length - byte_length;
+
+                    self.internal_read(offset, &mut chunk[..length])?;
+
+                    // Read not aligned rest of data
+                    if byte_length > 0 {
+                        let mut word_buffer = MaybeUninit::<FlashWordBuffer>::uninit();
+                        let word_buffer = unsafe { word_buffer.assume_init_mut() };
+
+                        self.internal_read(offset + length as u32, &mut word_buffer[..])?;
+                        chunk[length..].copy_from_slice(&word_buffer[..byte_length]);
+                    }
+                }
+            }
+        } else {
+            // Bytes buffer isn't word-aligned so we might read only via aligned buffer
+            let mut buffer = MaybeUninit::<FlashSectorBuffer>::uninit();
+            let buffer = unsafe { buffer.assume_init_mut() };
+
+            for (offset, chunk) in (offset..)
+                .step_by(Self::SECTOR_SIZE as _)
+                .zip(bytes.chunks_mut(Self::SECTOR_SIZE as _))
+            {
+                // Read to temporary buffer first (chunk length is aligned)
+                #[cfg(not(feature = "bytewise-read"))]
+                self.internal_read(offset, &mut buffer[..chunk.len()])?;
+
+                // Read to temporary buffer first (chunk length is not aligned)
+                #[cfg(feature = "bytewise-read")]
+                {
+                    let length = chunk.len();
+                    let byte_length = length % Self::WORD_SIZE as usize;
+                    let length = if byte_length > 0 {
+                        length - byte_length + Self::WORD_SIZE as usize
+                    } else {
+                        length
+                    };
+
+                    self.internal_read(offset, &mut buffer[..length])?;
+                }
+
+                // Copy to bytes buffer
+                chunk.copy_from_slice(&buffer[..chunk.len()]);
+            }
+        }
+
+        Ok(())
+    }
+
+    fn capacity(&self) -> usize {
+        self.capacity
+    }
+}
+
+impl NorFlash for FlashStorage {
+    const WRITE_SIZE: usize = Self::WORD_SIZE as _;
+    const ERASE_SIZE: usize = Self::SECTOR_SIZE as _;
+
+    fn write(&mut self, offset: u32, bytes: &[u8]) -> Result<(), Self::Error> {
+        self.check_alignment::<{ Self::WORD_SIZE }>(offset, bytes.len())?;
+        self.check_bounds(offset, bytes.len())?;
+
+        if Self::is_word_aligned(bytes) {
+            // Bytes buffer is word-aligned so we can write directly from it
+            for (offset, chunk) in (offset..)
+                .step_by(Self::SECTOR_SIZE as _)
+                .zip(bytes.chunks(Self::SECTOR_SIZE as _))
+            {
+                // Chunk already is word aligned so we can write directly from it
+                self.internal_write(offset, chunk)?;
+            }
+        } else {
+            // Bytes buffer isn't word-aligned so we might write only via aligned buffer
+            let mut buffer = MaybeUninit::<FlashSectorBuffer>::uninit();
+            let buffer = unsafe { buffer.assume_init_mut() };
+
+            for (offset, chunk) in (offset..)
+                .step_by(Self::SECTOR_SIZE as _)
+                .zip(bytes.chunks(Self::SECTOR_SIZE as _))
+            {
+                // Copy to temporary buffer first
+                buffer[..chunk.len()].copy_from_slice(chunk);
+                // Write from temporary buffer
+                self.internal_write(offset, &buffer[..chunk.len()])?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn erase(&mut self, from: u32, to: u32) -> Result<(), Self::Error> {
+        let len = (to - from) as _;
+        self.check_alignment::<{ Self::SECTOR_SIZE }>(from, len)?;
+        self.check_bounds(from, len)?;
+
+        for sector in from / Self::SECTOR_SIZE..to / Self::SECTOR_SIZE {
+            self.internal_erase(sector)?;
+        }
+
+        Ok(())
+    }
+}
